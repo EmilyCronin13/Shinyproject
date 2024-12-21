658b5a7790ee3d0cd33b1bf63dfea5a36aa1ab14
Your Name <E.cronin15@universityofgalway.ie>
2024-12-21 17:48:37 +0000

    sending another basic ui test

--- app.R
+++ app.R
@@ -1,21 +1,43 @@
-
 library(shiny)
+library(tidyverse)
 
 ui <- fluidPage(
-  titlePanel("Testing shiny !"),
+  titlePanel("DIG Study"),
+  
   sidebarLayout(
     sidebarPanel(
-      sliderInput("slider", "Select a value:", 1, 100, 50)
+      sliderInput("age_range", "Age Range", min = 30, max = 100, value = c(30, 100)),
+      selectInput("gender", "Gender", choices = c("All", "Male", "Female"), selected = "All")
     ),
     mainPanel(
-      textOutput("value")
+      textOutput("total_study_patients"),
+      textOutput("avg_age"),
+      textOutput("total_patients")
     )
   )
 )
 
 server <- function(input, output) {
-  output$value <- renderText({
-    paste("You selected:", input$slider)
+  full_data <- tibble(AGE = sample(30:100, 1000, replace = TRUE), SEX = sample(1:2, 1000, replace = TRUE))
+  
+  filtered_data <- reactive({
+    data <- full_data %>% filter(AGE >= input$age_range[1] & AGE <= input$age_range[2])
+    if (input$gender != "All") {
+      data <- data %>% filter(SEX == ifelse(input$gender == "Male", 1, 2))
+    }
+    data
+  })
+  
+  output$total_study_patients <- renderText({
+    paste("Total Patients in Study:", nrow(full_data))
+  })
+  
+  output$total_patients <- renderText({
+    paste("Filtered Patients:", nrow(filtered_data()))
+  })
+  
+  output$avg_age <- renderText({
+    paste("Average Age (Filtered):", round(mean(filtered_data()$AGE, na.rm = TRUE), 1))
   })
 }
 
